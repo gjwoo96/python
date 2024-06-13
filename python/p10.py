6d954472eb0d831aeb1eba96cffafbb1257461c6
gjwoo960101 <gjwoo96@nayuntech.com>
2024-06-13 17:51:00 +0900

    Update p10.py

--- python/p10.py
+++ python/p10.py
@@ -11,6 +11,14 @@ def checkpoint(soldiers):
     gun = gun - soldiers
     print ("[함수 내] 남은 총 : {0}".format(gun))
 
+def checkpoint_ret(gun,soldiers):
+    # 지역함수 사용
+    gun = gun - soldiers
+    print ("[함수 내] 남은 총 : {0}".format(gun))
+    return gun
+
 print("전체 총 : {0}".format(gun))
-checkpoint(2)
+#checkpoint(2)
 print("남은 총 : {0}".format(gun))
+# 리턴받은 지역함수를 전역함수에 저장
+gun = checkpoint_ret(gun , 2)
